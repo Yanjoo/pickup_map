19bc51e87c288e25e611d26ff565b38475e619fe
Yanjoo <hanjub3@gmail.com>
2020-10-24 22:15:20 +0900

    시작 좌표, 끝 좌표 추가

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
+from .models import Point
 from .algorithms import *
+
 def checkLocation(code):
 
     # 코드 
@@ -8,15 +10,24 @@ def checkLocation(code):
     return 
 
 def index(request):
-    paths = findPath(1102, 2101)
+    start = 1102 # 시작 점
+    end = 3101 # 종료점
+    paths = findPath(start, end)
     print('=========')
-    print(paths)
-    points_list = []
+    print('경로', paths)
+    points_list = [] # 중간 경로 좌표
     for i in paths:
         pt = get_object_or_404(Point, pk=i)
         points_list.append([pt.locationX, pt.locationY, pt.locationZ])
-    print(points_list)    
     
+    start_point = get_object_or_404(Point, id=start)
+    end_point = get_object_or_404(Point, id=end)
+    point_start = [start_point.locationX, start_point.locationY, start_point.locationZ]
+    point_end = [end_point.locationX, end_point.locationY, end_point.locationZ]
+    
+    print('중간 좌표', points_list)
+    print('시작 좌표', point_start)
+    print('종료 좌표', point_end)
 
     if request.is_ajax():
         code = request.GET.get('code','')
